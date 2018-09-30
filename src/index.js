9863b25cf8e4f67d892cb2086076c9ec1b2e5b96
samo-war <samo_war@mail.ru>
2018-09-30 21:26:15 +0300

    Task 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  const divs = [];
+  let multiplier = 1;
+
+  while (divs.slice(-1) && divs.slice(-1) <= number) {
+    divs.push(Math.pow(5, multiplier++));
+  }
+
+  return divs.reduce((acc, cur) => acc + Math.floor(number / cur), 0);
 }
